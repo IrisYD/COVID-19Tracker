80c1eedff107ceff6c9921bfcde785d77320098a
firebearrex <60494405+firebearrex@users.noreply.github.com>
2022-02-21 23:27:04 -0800

    Add Table and LineGraph

---
+++ src/Table.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './Table.css';
+
+function Table({countries}) {
+  return <div className='table'>
+        {countries.map( ({country, cases}) => (
+            <tr>
+                <td>{country}</td>
+                <td>
+                    <strong>{cases}</strong>
+                </td>   
+            </tr>
+
+        ))}
+      </div>
+  
+}
+
+export default Table
