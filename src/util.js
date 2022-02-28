ac4e193204649a3f0cc5667817edc96d0394e87b
firebearrex <60494405+firebearrex@users.noreply.github.com>
2022-02-28 01:07:41 -0800

    Fix bug - country dropdown
    Selecting countries but data do not change.

--- src/util.js
+++ src/util.js
@@ -1,5 +1,4 @@
 export const sortData = (data) => {
-    const sortedData = [...data];
-    return sortedData.sort((a, b) => (a.cases > b.cases ? -1:1));
-
+    // const sortedData = [...data];
+    return [...data].sort((a, b) => (a.cases > b.cases ? -1 : 1));
 }
