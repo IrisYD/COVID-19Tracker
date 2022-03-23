d2b6415380e0aebeb74d3acbb85c599ff1d9a42b
YiDeng <ellie.yideng@gmail.com>
2022-03-22 19:54:16 -0700

    modified the community page

--- src/pages/Community.js
+++ src/pages/Community.js
@@ -175,7 +175,7 @@ function Community() {
 function Card() {
     return <li>
     <div className='user'>
-        <div className='avatar' style={{backgroundImage: 'url(http://localhost:3000/coronavirus_icon.png)'}} />
+        <div className='avatar' style={{backgroundImage: 'url(../images/spiderman.png)'}} />
         <div className='user_name'>Wonder Women</div>
         <div>Age: 28</div>
         <div>No underlying disease</div>
@@ -205,52 +205,5 @@ function Card() {
     </div>
 </li>
 }
-
-const emails = ['username@gmail.com', 'user02@gmail.com'];
-
-function SimpleDialog(props) {
-  const { onClose, selectedValue, open } = props;
-
-  const handleClose = () => {
-    onClose(selectedValue);
-  };
-
-  const handleListItemClick = (value) => {
-    onClose(value);
-  };
-
-  return (
-    <Dialog onClose={handleClose} open={open}>
-      <DialogTitle>Set backup account</DialogTitle>
-      <List sx={{ pt: 0 }}>
-        {emails.map((email) => (
-          <ListItem button onClick={() => handleListItemClick(email)} key={email}>
-            <ListItemAvatar>
-              <Avatar sx={{ bgcolor: blue[100], color: blue[600] }}>
-                <PersonIcon />
-              </Avatar>
-            </ListItemAvatar>
-            <ListItemText primary={email} />
-          </ListItem>
-        ))}
-
-        <ListItem autoFocus button onClick={() => handleListItemClick('addAccount')}>
-          <ListItemAvatar>
-            <Avatar>
-              <AddIcon />
-            </Avatar>
-          </ListItemAvatar>
-          <ListItemText primary="Add account" />
-        </ListItem>
-      </List>
-    </Dialog>
-  );
-}
-
-SimpleDialog.propTypes = {
-  onClose: PropTypes.func.isRequired,
-  open: PropTypes.bool.isRequired,
-  selectedValue: PropTypes.string.isRequired,
-};
 
 export default Community
