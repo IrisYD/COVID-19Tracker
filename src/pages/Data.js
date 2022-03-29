e71cb17b6e7f1c59f71cfc67ad4cb6c7f5e89d8a
Suying <liu.suyi@northeastern.edu>
2022-03-29 00:58:01 -0700

    Update map size and zoom ratio

--- src/pages/Data.js
+++ src/pages/Data.js
@@ -14,7 +14,7 @@ function Data() {
     const [countryInfo, setCountryInfo] = useState({});
     const [tableData, setTableData] = useState([]);
     const [mapCenter, setMapCenter] = useState({lat: 34.80746, lng: -40.4796});
-    const [mapZoom, setMapZoom] = useState(3);
+    const [mapZoom, setMapZoom] = useState(3.5);
     const [mapCountries, setMapCountries] = useState([]);
     const [casesType, setCasesType] = useState("cases");
 
@@ -29,7 +29,6 @@ function Data() {
     useEffect(() => {
         const getCountriesData = async () => {
             await fetch("https://disease.sh/v3/covid-19/countries")
-            // fetch("https://disease.sh/v3/covid-19/countries")
                 .then((response) => response.json())
                 .then((data) => {
                     const countries = data.map((country) => (
@@ -71,58 +70,55 @@ function Data() {
 
             <div className='app-data'>
 
-            <div className='app-data__left'>
-                <div className='app-data__stats'>
-                    <InfoBox
-                        // onClick={(e) => setCasesType("cases")}
-                        title="COVID Cases"
-                        isRed
-                        active={casesType === "cases"}
-                        cases={countryInfo.todayCases}
-                        total={countryInfo.cases}/>
-                    <InfoBox
-                        // onClick={(e) => setCasesType("recovered")}
-                        title="Recovered"
-                        active={casesType === "recovered"}
-                        cases={countryInfo.todayRecovered}
-                        total={countryInfo.recovered}/>
-                    <InfoBox
-                        // onClick={(e) => setCasesType("deaths")}
-                        title="Death"
-                        isRed
-                        active={casesType === "deaths"}
-                        cases={countryInfo.todayDeaths}
-                        total={countryInfo.deaths}/>
-                    <FormControl className="app-data__dropdown">
-                        <Select variant="outlined" value={country} onChange={onCountryChange}>
-                            <MenuItem value="worldwide">Worldwide</MenuItem>
-                            {
-                                countries.map(country => (
-                                    <MenuItem value={country.value}>{country.name}</MenuItem>
-                                ))
-                            }
-                        </Select>
-                    </FormControl>
+                <div className='app-data__left'>
+                    <div className='app-data__stats'>
+                        <InfoBox
+                            title="COVID Cases"
+                            isRed
+                            active={casesType === "cases"}
+                            cases={countryInfo.todayCases}
+                            total={countryInfo.cases}/>
+                        <InfoBox
+                            title="Recovered"
+                            active={casesType === "recovered"}
+                            cases={countryInfo.todayRecovered}
+                            total={countryInfo.recovered}/>
+                        <InfoBox
+                            title="Death"
+                            isRed
+                            active={casesType === "deaths"}
+                            cases={countryInfo.todayDeaths}
+                            total={countryInfo.deaths}/>
+                        <FormControl className="app-data__dropdown">
+                            <Select variant="outlined" value={country} onChange={onCountryChange}>
+                                <MenuItem value="worldwide">Worldwide</MenuItem>
+                                {
+                                    countries.map(country => (
+                                        <MenuItem value={country.value}>{country.name}</MenuItem>
+                                    ))
+                                }
+                            </Select>
+                        </FormControl>
+                    </div>
+                    <Map
+                        countries={mapCountries}
+                        casesType={casesType}
+                        center={mapCenter}
+                        zoom={mapZoom}
+                    />
                 </div>
-                <Map
-                    countries={mapCountries}
-                    casesType={casesType}
-                    center={mapCenter}
-                    zoom={mapZoom}
-                />
-            </div>
 
-            <div className='app-data__right'>
-            <Card >
-                <CardContent>
-                    <h3>Live Cases By Country</h3>
-                    <Table countries={tableData}/>
-                    <h3>World wide new cases</h3>
-                    <LineGraph/>
-                </CardContent>
+                <div className='app-data__right'>
+                    <Card >
+                        <CardContent>
+                            <h3>Live Cases By Country</h3>
+                            <Table countries={tableData}/>
+                            <h3>World wide new cases</h3>
+                            <LineGraph/>
+                        </CardContent>
 
-            </Card>
-            </div>
+                    </Card>
+                </div>
             </div>
 
     );
