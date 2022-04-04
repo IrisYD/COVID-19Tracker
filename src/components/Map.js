40cb598efa7851fbeb78ae638e89010a9ed2489f
Suying <liu.suyi@northeastern.edu>
2022-04-04 15:30:17 -0700

    Update zoom in/out and drag function in Map Container on data page

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,26 +1,31 @@
 import React from 'react';
 import "./Map.css"
 import { MapContainer as LeafletMap, TileLayer, useMap} from "react-leaflet";
+import Leaflet from 'leaflet';
 import {showDataOnMap} from "../util";
 
 function Map({countries, casesType, center, zoom}) {
-  return (
-    <div className='map'>
-        <LeafletMap center={center} zoom={zoom}>
-            <TileLayer
-                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-                attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-            />
-            <ChangeMapView center={center} zoom={zoom} />
-            {showDataOnMap(countries, casesType)}
-        </LeafletMap>
-    </div>
+    const southWest = Leaflet.latLng(-90, -260)
+    const northEast = Leaflet.latLng(90, 260)
+    const bounds = Leaflet.latLngBounds(southWest, northEast)
+    return (
+        <div className='map'>
+            <LeafletMap center={center} zoom={zoom} maxBounds={bounds} maxBoundsViscosity={1.0}>
+                <TileLayer
+                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                    attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+                />
+                <ChangeMapView center={center} zoom={zoom}/>
+                {showDataOnMap(countries, casesType)}
+            </LeafletMap>
+        </div>
   );
 }
 
 function ChangeMapView({ center, zoom }) {
     const map = useMap();
     map.setView(center, zoom);
+    map.options.minZoom = 1.5;
     return null;
 }
 
